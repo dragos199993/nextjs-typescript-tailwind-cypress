5f0af67f9746a20231e4ad26fed817f7e3167c9c
Dragos Nedelcu <dragos.nedelcu@pitechplus.com>
2021-11-22 20:35:52 +0200

    Added prettier.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      Next saas
-    </div>
-  )
+  return <div>Next saas</div>
 }
 
 export default Home
